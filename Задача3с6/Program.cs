b6c780d2bb12b27f9a7c958821c9df122df50e09
MarinaPorokhnya <marina.vbyaby.poroxnya@mail.ru>
2024-07-08 12:49:08 +0300

    Добавили решение задача3с6

---
+++ Задача3с6/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string Reverser(string a)
+{
+    char[] arr = a.ToCharArray();
+    Array.Reverse(arr);
+    return new string(arr);
+}
+string Palindromer(string x)
+{
+    string y = Reverser(x);
+    Console.WriteLine(y);
+    if (Equals(x, y))
+        return "Палиндром";
+    else
+        return "Не палиндром";
+}
+
+Console.WriteLine("Введите тестируемую строку");
+string input = Console.ReadLine();
+string str = input.ToLower();
+Console.WriteLine(input + " - " + Palindromer(str));
+Console.WriteLine();
